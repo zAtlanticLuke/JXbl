19af7b5ba6955cf5ad184dcd3509f9212ba58f4b
zAtlanticLuke <stuluc19@htl-kaindorf.at>
2023-06-05 06:50:44 +0200

    First commit

---
+++ Backend/src/main/java/com/jxbl/jxbl/HelloResource.java
@@ -0,0 +1,16 @@
+package com.jxbl.jxbl;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+
+@Path("/hello-world")
+public class HelloResource
+{
+    @GET
+    @Produces("text/plain")
+    public String hello()
+    {
+        return "Hello, World!";
+    }
+}
